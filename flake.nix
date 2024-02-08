71b63888733e54a3b009ca6be16b7b0fa23228cb
Michalis Pardalos <me@mpardalos.com>
2024-02-08 15:38:53 +0000

    Set up a dune project

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A verified verilog equivalence checker (minimum viable product)";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        coq = pkgs.coq_8_18;
+        coqPackages = pkgs.coqPackages_8_18;
+      in {
+        devShells.default = pkgs.mkShell {
+          packages =
+            [
+              coq
+              coq.ocaml
+              coq.ocamlPackages.findlib
+              coq.ocamlPackages.ocaml-lsp
+              coq.ocamlPackages.dune_3
+
+              pkgs.sv-lang
+            ];
+        };
+      }
+    );
+}
