edd267fe5f3792eb350a2f17b2977790ba183e32
Michalis Pardalos <me@mpardalos.com>
2024-02-09 21:33:27 +0000

    Extract all of VVEquiv

--- bin/Extraction.v
+++ bin/Extraction.v
@@ -6,5 +6,4 @@ From Coq Require Import extraction.ExtrOcamlZInt.
 
 Extraction Language OCaml.
 
-Extract Constant VVEquiv.add1 => "Helpers.add1".
-Extraction "VVEquiv.ml" VVEquiv.program_name VVEquiv.a_num.
+Extraction Library VVEquiv.
