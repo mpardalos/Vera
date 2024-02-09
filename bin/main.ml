e22a23f49d6a69f2fb5e5dd4678b3407f65cf7c0
Michalis Pardalos <me@mpardalos.com>
2024-02-09 23:50:52 +0000

    Parsing of addition, literals, and conversions

--- bin/main.ml
+++ bin/main.ml
@@ -2,6 +2,65 @@
 (*   List.fold_left (fun (s : string) (c : char) -> s ^ (String.make 1 c)) "" lst; *)
 (* ;; *)
 
+module FromJson = struct
+  open VVEquiv
+
+  exception UnexpectedJson of (string * Yojson.Safe.t)
+  exception UnexpectedKind of string
+  exception UnknownType of string
+  exception UnknownOp of string
+
+  let missing_key ((k : string), (j : Yojson.Safe.t)) =
+    UnexpectedJson (Printf.sprintf "No key '%s'" k, j)
+
+  let assoc_lookup (key : 'a) (assoc : ('a * 'b) list) : 'b option =
+    Option.map snd (List.find_opt (fun (k, _) -> k == key) assoc)
+
+  let get_key key = function
+    | `Assoc lst as json -> (
+        match assoc_lookup key lst with
+        | Some x -> x
+        | None -> raise (missing_key (key, json)))
+    | json -> raise (missing_key (key, json))
+
+  let as_string = function
+    | `String s -> s
+    | json -> raise (UnexpectedJson ("string", json))
+
+  let as_int = function
+    | `Int n -> n
+    | json -> raise (UnexpectedJson ("int", json))
+
+  let read_op = function "Add" -> Verilog.Plus | op -> raise (UnknownOp op)
+
+  let read_type (s : string) : Verilog.vtype =
+    Scanf.sscanf s "%s@[%d, %d]" (fun t n1 n2 ->
+        match t with
+        | "reg" -> Verilog.Reg (n1, n2)
+        | "logic" -> Verilog.Logic (n1, n2)
+        | _ -> raise (UnknownType s))
+
+  let read_value (s : string) : int =
+    try Scanf.sscanf s "%d'd%d" (fun _length value -> value)
+    with Scanf.Scan_failure _ -> Scanf.sscanf s "%d" (fun value -> value)
+
+  let rec expression (json : Yojson.Safe.t) : Verilog.expression =
+    match as_string (get_key "kind" json) with
+    | "BinaryOp" ->
+        let op = read_op (as_string (get_key "op" json)) in
+        let left_json = get_key "left" json in
+        let right_json = get_key "right" json in
+        Verilog.BinaryOp (op, expression left_json, expression right_json)
+    (* | "NamedValue" -> _ *)
+    | "Conversion" ->
+        let operand_json = get_key "operand" json in
+        let type_str = as_string (get_key "type" json) in
+        Verilog.Conversion (read_type type_str, expression operand_json)
+    | "IntegerLiteral" ->
+        Verilog.IntegerLiteral (read_value (as_string (get_key "value" json)))
+    | k -> raise (UnexpectedKind k)
+end
+
 let usage_msg = "vvequiv <file.sv>"
 let input_file = ref ""
 
--- vvequiv/VVEquiv.v
+++ vvequiv/VVEquiv.v
@@ -1,3 +1,17 @@
 Require Import String.
 Require Import ZArith.
 
+Module Verilog.
+  Inductive vtype :=
+  | Reg : Z -> Z -> vtype
+  | Logic : Z -> Z -> vtype
+  .
+
+  Inductive op := Plus | Minus.
+
+  Inductive expression :=
+  | BinaryOp : op -> expression -> expression -> expression
+  | Conversion : vtype -> expression -> expression
+  | IntegerLiteral : Z -> expression
+  .
+End Verilog.
