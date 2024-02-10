0ade40fc03d9e539720dbcd82b4aed4dc1161b7e
Michalis Pardalos <me@mpardalos.com>
2024-02-10 15:29:01 +0000

    WIP top-level parsing

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,5 @@
-(* let lst_to_string (lst : char list) : string = *)
-(*   List.fold_left (fun (s : string) (c : char) -> s ^ (String.make 1 c)) "" lst; *)
-(* ;; *)
+let lst_to_string (lst : char list) : string =
+  List.fold_left (fun (s : string) (c : char) -> s ^ String.make 1 c) "" lst
 
 module FromJson = struct
   open VVEquiv
@@ -19,46 +18,63 @@ module FromJson = struct
   let get_key key = function
     | `Assoc lst as json -> (
         match assoc_lookup key lst with
-        | Some x -> x
-        | None -> raise (missing_key (key, json)))
-    | json -> raise (missing_key (key, json))
-
-  let as_string = function
-    | `String s -> s
-    | json -> raise (UnexpectedJson ("string", json))
+        | Some x -> x    | json -> raise (UnexpectedJson ("string", json))
 
   let as_int = function
     | `Int n -> n
     | json -> raise (UnexpectedJson ("int", json))
+
+  let as_list = function
+    | `List l -> l
+    | json -> raise (UnexpectedJson ("list", json))
 
   let read_op = function "Add" -> Verilog.Plus | op -> raise (UnknownOp op)
 
   let read_type (s : string) : Verilog.vtype =
     Scanf.sscanf s "%s@[%d, %d]" (fun t n1 n2 ->
         match t with
-        | "reg" -> Verilog.Reg (n1, n2)
-        | "logic" -> Verilog.Logic (n1, n2)
+        | "reg" | "logic" -> Verilog.Logic (n1, n2)
         | _ -> raise (UnknownType s))
 
   let read_value (s : string) : int =
     try Scanf.sscanf s "%d'd%d" (fun _length value -> value)
     with Scanf.Scan_failure _ -> Scanf.sscanf s "%d" (fun value -> value)
 
-  let rec expression (json : Yojson.Safe.t) : Verilog.expression =
+  let rec read_expression (json : Yojson.Safe.t) : Verilog.expression =
     match as_string (get_key "kind" json) with
     | "BinaryOp" ->
         let op = read_op (as_string (get_key "op" json)) in
         let left_json = get_key "left" json in
         let right_json = get_key "right" json in
-        Verilog.BinaryOp (op, expression left_json, expression right_json)
+        Verilog.BinaryOp
+          (op, read_expression left_json, read_expression right_json)
     (* | "NamedValue" -> _ *)
     | "Conversion" ->
         let operand_json = get_key "operand" json in
         let type_str = as_string (get_key "type" json) in
-        Verilog.Conversion (read_type type_str, expression operand_json)
+        Verilog.Conversion (read_type type_str, read_expression operand_json)
     | "IntegerLiteral" ->
         Verilog.IntegerLiteral (read_value (as_string (get_key "value" json)))
     | k -> raise (UnexpectedKind k)
+
+  let read_module (json : Yojson.Safe.t) : Verilog.vmodule =
+    { modName = []; modPorts = []; modVariables = [] }
+
+  let read_ast (json : Yojson.Safe.t) : Verilog.vmodule list =
+    let members = as_list (get_key "members" json) in
+    List.filter_map
+      (fun m ->
+        let kind = as_string (get_key "kind" m) in
+        let body = get_key "body" m in
+        if kind == "Instance" then Some (read_module body) else None)
+      members
+end
+
+module VerilogPP = struct
+  open VVEquiv
+
+  let vmodule (fmt : Format.formatter) (m : Verilog.vmodule) =
+    Format.pp_print_string fmt (lst_to_string m.modName)
 end
 
 let usage_msg = "vvequiv <file.sv>"
@@ -80,4 +96,8 @@ let () =
       (Printf.sprintf "slang --quiet --ast-json - %s" !input_file)
   in
   let ast_json = Yojson.Safe.from_string ast_json_str in
-  Format.printf "%a\n" Yojson.Safe.pp ast_json
+  try
+    let modules = FromJson.read_ast ast_json in
+    Format.printf "%a\n" (Format.pp_print_list VerilogPP.vmodule) modules;
+  with FromJson.UnexpectedJson (msg, json) ->
+    Format.printf "Unexpected json: %s\n%a" msg Yojson.Safe.pp json
--- vvequiv/VVEquiv.v
+++ vvequiv/VVEquiv.v
@@ -2,10 +2,9 @@ Require Import String.
 Require Import ZArith.
 
 Module Verilog.
-  Inductive vtype :=
-  | Reg : Z -> Z -> vtype
-  | Logic : Z -> Z -> vtype
-  .
+  Inductive vtype := Logic : Z -> Z -> vtype.
+
+  Inductive direction := In | Out.
 
   Inductive op := Plus | Minus.
 
@@ -14,4 +13,25 @@ Module Verilog.
   | Conversion : vtype -> expression -> expression
   | IntegerLiteral : Z -> expression
   .
+
+  Record variable :=
+    MkVariable
+      { varName : string
+      ; varType : vtype
+      ; varId : Z
+      }.
+
+  Record port :=
+    MkPort
+      { portId : Z
+      ; portDirection : direction
+      }.
+
+  (** Verilog modules *)
+  Record vmodule : Set :=
+    MkMod
+      { modName : string
+      ; modPorts : list port
+      ; modVariables : list variable
+      }.
 End Verilog.
