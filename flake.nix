f7ef2a8f5cd19359b8965112bf1d09a2527f24e2
Michalis Pardalos <me@mpardalos.com>
2024-03-06 16:25:09 +0000

    Getting cohpred to actually compile

--- flake.nix
+++ flake.nix
@@ -1,35 +1,30 @@
 {
-  description = "A verified verilog equivalence checker (minimum viable product)";
+  description = "CohPred dependencies";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs"; };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        coq = pkgs.coq_8_18;
-        coqPackages = pkgs.coqPackages_8_18;
-      in {
-        devShells.default = pkgs.mkShell {
-          packages =
-            [
-              coq
-              coqPackages.coq-ext-lib
-
-              coq.ocaml
-              coq.ocamlPackages.findlib
-              coq.ocamlPackages.ocaml-lsp
-              coq.ocamlPackages.dune_3
-              coq.ocamlPackages.utop
-              coq.ocamlPackages.ocamlformat
-
-              pkgs.sv-lang
-              pkgs.verible
-            ];
-        };
-      }
-    );
+  outputs = { self, nixpkgs }:
+    let vericertDevPackages = pkgs:
+          let
+            veriT' = pkgs.veriT.overrideAttrs (oA: {
+              src = pkgs.fetchurl {
+                url = "https://www.lri.fr/~keller/Documents-recherche/Smtcoq/veriT9f48a98.tar.gz";
+                sha256 = "sha256-Pe46PxQVHWwWwx5Ei4Bl95A0otCiXZuUZ2nXuZPYnhY=";
+              };
+              meta.broken = false;
+            });
+            ncoq = pkgs.coq_8_17;
+            ncoqPackages = pkgs.coqPackages_8_17;
+          in
+            pkgs.mkShell {
+              buildInputs = with pkgs;
+                [ ncoq ncoq.ocaml ncoqPackages.serapi dune_3 gcc python3 lp_solve veriT' zchaff ]
+		++ (with ncoqPackages; [coq-ext-lib])
+                ++ (with ncoq.ocamlPackages; [ findlib menhir menhirLib ocamlgraph ocp-indent utop merlin ])
+                ++ (with python3Packages; [ alectryon sphinx_rtd_theme ]);
+            };
+    in {
+      devShell.x86_64-linux = vericertDevPackages nixpkgs.legacyPackages.x86_64-linux;
+      devShell.x86_64-darwin = vericertDevPackages nixpkgs.legacyPackages.x86_64-darwin;
+    };
 }
