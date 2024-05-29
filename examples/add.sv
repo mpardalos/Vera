75f217f8a0960f51f4d7035a462289c2a7160837
Michalis Pardalos <me@mpardalos.com>
2024-05-29 15:44:58 +0100

    Playing around with slang examples

--- examples/add.sv
+++ examples/add.sv
@@ -1,3 +1,5 @@
-module add(input reg [31:0] x, input reg [31:0] y, output wire [31:0] out);
-   assign out = x + y;
+module add(input wire [31:0] x, input wire [31:0] y, output wire [31:0] out);
+   wire [15:0] v;
+   assign v = x;
+   assign out = v + y;
 endmodule // add
