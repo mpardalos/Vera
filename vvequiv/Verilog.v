0db7c8ed01ebb4b43297d1baed2d6374eb7372db
Michalis Pardalos <me@mpardalos.com>
2024-06-25 09:03:06 +0100

    Unify typed and untyped verilog

--- vvequiv/Verilog.v
+++ vvequiv/Verilog.v
@@ -1,6 +1,6 @@
 From vvequiv Require Import Common.
 From vvequiv Require Import Bitvector.
-Import Bitvector (bv).
+Import Bitvector (bv, mkBV).
 
 Require Import String.
 Require Import ZArith.
@@ -13,9 +13,12 @@ From Equations Require Import Equations.
 
 (* This module will be Verilog.Verilog. Redundant, but it is needed for extraction. See Extraction.v *)
 Module Verilog.
-  Inductive vtype := Logic : N -> N -> vtype.
+  Set Implicit Arguments.
+  Set Strict Implicit.
 
-  Equations Derive NoConfusionHom EqDec for vtype.
+  Inductive VType := Logic : N -> N -> VType.
+
+  Equations Derive NoConfusionHom EqDec for VType.
   Next Obligation.
     destruct x as [hi1 lo1].
     destruct y as [hi2 lo2].
@@ -31,38 +34,86 @@ Module Verilog.
 
   Inductive BinaryOperator := Plus | Minus.
 
-  Inductive expression :=
-  | BinaryOp : BinaryOperator -> expression -> expression -> expression
-  | IntegerLiteral : positive -> N -> expression
-  | NamedExpression : string -> expression
+  (* Equations bv_type (idx : Type) (bv : ) *)
+  Structure VerilogKind :=
+    { t :> Type
+    ; bv_type : Bitvector.bv -> t
+    }.
+
+  Definition Typed : VerilogKind :=
+    {| t := VType
+    ; bv_type v := Verilog.Logic (N.pred (N.pos (Bitvector.width v))) 0
+    |}.
+
+  Definition Untyped : VerilogKind :=
+    {| t := unit
+    ; bv_type _ := tt
+    |}.
+
+  Arguments bv_type {_} & _.
+
+  Inductive Expression (k : VerilogKind) : k -> Type :=
+  | BinaryOp :
+    forall {t}
+      (op : BinaryOperator)
+      (lhs rhs: Expression k t),
+      Expression k t
+  | Conversion :
+    forall (from_type to_type : k)
+      (operand: Expression k from_type),
+      Expression k to_type
+  | IntegerLiteral :
+    forall (value : Bitvector.bv),
+      Expression k (bv_type value)
+  | VarRef :
+    forall (type : k)
+      (name : string),
+      Expression k type
   .
 
-  Record variable :=
-    MkVariable
-      { varType : vtype
-      ; varName : string
-      }.
+  Definition SomeExpression := { k : VerilogKind & { t : k & Expression k t}}.
+  Definition TypedExpression (t : VType) := Expression Typed t.
+  Definition SomeTypedExpression := { t : VType & TypedExpression t }.
+  Definition UntypedExpression := Expression Untyped tt.
 
-  Record port :=
+  Notation UntypedVarRef name := (VarRef Untyped tt name).
+  Notation TypedIntegerLiteral t v := (IntegerLiteral Typed t v).
+  Notation UntypedIntegerLiteral v := (IntegerLiteral Untyped tt v).
+
+  Inductive Statement (k : VerilogKind) :=
+  | Block (body : list (Statement k))
+  | BlockingAssign {t : k} (lhs rhs : Expression k t)
+  | NonBlockingAssign {t : k} (lhs rhs : Expression k t)
+  | If {t : k} (condition : Expression k t) (trueBranch falseBranch : (Statement k))
+  .
+
+  Inductive ModuleItem (k: VerilogKind) :=
+  | ContinuousAssign {t : k} (lhs rhs : Expression k t)
+  | AlwaysFF (body : Statement k)
+  .
+
+  Record Port :=
     MkPort
       { portDirection : port_direction
       ; portName : string
       }.
 
-  Inductive module_item : Set :=
-  | ContinuousAssign : expression -> expression -> module_item
-  .
-
-  (** Verilog modules *)
-  Record vmodule : Set :=
-    MkMod
-      { modName : string
-      ; modPorts : list port
-      ; modVariables : list variable
-      ; modBody : list module_item
+  Record Var :=
+    MkVar
+      { varType : VType
+      ; varName : string
       }.
 
-  Example examples : list (vmodule * vmodule) :=
+  (** Verilog modules *)
+  Record VerilogModule (k: VerilogKind) :=
+    MkVerilogModule
+      { modName : string
+      ; modPorts : list Port
+      ; modVariables : list Var
+      ; modBody : list (ModuleItem k)
+      }.
+
+  Example examples : list (VerilogModule Untyped * VerilogModule Untyped) :=
     let l32 := Logic 31 0 in
     let l16 := Logic 15 0 in
     [
@@ -73,13 +124,13 @@ Module Verilog.
             MkPort PortOut "out"
           ];
           modVariables := [
-            MkVariable l32 "in" ;
-            MkVariable l32 "out"
+            MkVar l32 "in" ;
+            MkVar l32 "out"
           ];
           modBody := [
             ContinuousAssign
-              (NamedExpression "out")
-              (BinaryOp Plus (NamedExpression "in") (IntegerLiteral 32 0))
+              (UntypedVarRef "out")
+              (BinaryOp Plus (UntypedVarRef "in") (IntegerLiteral _ (mkBV 0 32)))
           ];
         |},
         {|
@@ -89,13 +140,13 @@ Module Verilog.
             MkPort PortOut "out"
           ];
           modVariables := [
-            MkVariable l32 "in" ;
-            MkVariable l32 "out"
+            MkVar l32 "in" ;
+            MkVar l32 "out"
           ];
           modBody := [
             ContinuousAssign
-              (NamedExpression "out")
-              (NamedExpression "in")
+              (UntypedVarRef "out")
+              (UntypedVarRef "in")
           ];
         |}
       ) ;
@@ -107,15 +158,15 @@ Module Verilog.
             MkPort PortOut "out"
           ];
           modVariables := [
-            MkVariable l32 "in" ;
-            MkVariable l32 "out"
+            MkVar l32 "in" ;
+            MkVar l32 "out"
           ];
           modBody := [
             ContinuousAssign
-              (NamedExpression "out")
+              (UntypedVarRef "out")
               (BinaryOp Plus
-                 (NamedExpression "in")
-                 (IntegerLiteral 32 1))
+                 (UntypedVarRef "in")
+                 (IntegerLiteral _ (mkBV 1 32)))
           ];
         |},
         {|
@@ -125,15 +176,15 @@ Module Verilog.
             MkPort PortOut "out"
           ];
           modVariables := [
-            MkVariable l32 "in" ;
-            MkVariable l32 "out"
+            MkVar l32 "in" ;
+            MkVar l32 "out"
           ];
           modBody := [
             ContinuousAssign
-              (NamedExpression "out")
+              (UntypedVarRef "out")
               (BinaryOp Plus
-                 (IntegerLiteral 32 1)
-                 (NamedExpression "in"))
+                 (IntegerLiteral _ (mkBV 1 32))
+                 (UntypedVarRef "in"))
           ];
         |}
       ) ;
@@ -146,22 +197,22 @@ Module Verilog.
             MkPort PortOut "out"
           ];
           modVariables := [
-            MkVariable l32 "in1" ;
-            MkVariable l32 "in2" ;
-            MkVariable l16 "v" ;
-            MkVariable l32 "out"
+            MkVar l32 "in1" ;
+            MkVar l32 "in2" ;
+            MkVar l16 "v" ;
+            MkVar l32 "out"
           ];
           modBody := [
             ContinuousAssign
-              (NamedExpression "v")
-              (NamedExpression "in1");
+              (UntypedVarRef "v")
+              (UntypedVarRef "in1");
             ContinuousAssign
-              (NamedExpression "out")
+              (UntypedVarRef "out")
               (BinaryOp Plus
-                 (NamedExpression "v")
+                 (UntypedVarRef "v")
                  (BinaryOp Plus
-                    (NamedExpression "in2")
-                    (IntegerLiteral 32 1)))
+                    (UntypedVarRef "in2")
+                    (IntegerLiteral _ (mkBV 1 32))))
           ];
         |},
         {|
@@ -172,85 +223,20 @@ Module Verilog.
             MkPort PortOut "out"
           ];
           modVariables := [
-            MkVariable l32 "in1" ;
-            MkVariable l32 "in2" ;
-            MkVariable l32 "out"
+            MkVar l32 "in1" ;
+            MkVar l32 "in2" ;
+            MkVar l32 "out"
           ];
           modBody := [
             ContinuousAssign
-              (NamedExpression "out")
+              (UntypedVarRef "out")
               (BinaryOp Plus
-                 (NamedExpression "in2")
+                 (UntypedVarRef "in2")
                  (BinaryOp Plus
-                    (NamedExpression "in2")
-                    (IntegerLiteral 32 1)))
+                    (UntypedVarRef "in2")
+                    (IntegerLiteral _ (mkBV 1 32))))
           ];
         |}
       )
     ].
 End Verilog.
-
-Module TypedVerilog.
-  Export Verilog(vtype(..), BinaryOperator(..), variable(..), port(..)).
-
-  (* Equations bv_type (idx : Type) (bv : ) *)
-  Structure VerilogKind :=
-    { t :> Type
-    ; bv_type : Bitvector.bv -> t
-    }.
-
-  Definition vtype_kind : VerilogKind :=
-    {| t := vtype
-    ; bv_type v := Verilog.Logic (N.pred (N.pos (Bitvector.width v))) 0
-    |}.
-
-  Definition unit_kind : VerilogKind :=
-    {| t := unit
-    ; bv_type _ := tt
-    |}.
-
-  Inductive Expression {kind : VerilogKind} : kind -> Type :=
-  | BinaryOp :
-    forall {t}
-      (op : BinaryOperator)
-      (lhs rhs: Expression t),
-      Expression t
-  | Conversion :
-    forall (from_type to_type : kind)
-      (operand: Expression from_type),
-      Expression to_type
-  | IntegerLiteral :
-    forall (value : Bitvector.bv),
-      Expression (bv_type kind value)
-  | NamedExpression :
-    forall (type : kind)
-      (name : string),
-      Expression type
-  .
-
-  Definition SomeExpression := { k : VerilogKind & { t : k & Expression t}}.
-  Definition TypedExpression (t : vtype) := @Expression vtype_kind t.
-  Definition SomeTypedExpression := { t : vtype & TypedExpression t }.
-  Definition UntypedExpression := @Expression unit_kind tt.
-
-  Inductive Statement :=
-  | Block (body : list Statement)
-  | BlockingAssign {k : VerilogKind} {t : k} (lhs rhs : Expression t)
-  | NonBlockingAssign {k : VerilogKind} {t : k} (lhs rhs : Expression t)
-  | If {k : VerilogKind} {t : k} (condition : Expression t) (trueBranch falseBranch : Statement)
-  .
-
-  Inductive ModuleItem :=
-  | ContinuousAssign {k : VerilogKind} {t : k} (lhs rhs : Expression t)
-  | AlwaysFF (body : Statement)
-  .
-
-  (** Verilog modules *)
-  Record vmodule :=
-    MkMod
-      { modName : string
-      ; modPorts : list port
-      ; modVariables : list variable
-      ; modBody : list ModuleItem
-      }.
-End TypedVerilog.
