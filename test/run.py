384533c5e648eaae056a656d3a2d9b0d4e31d2c2
Michalis Pardalos <me@mpardalos.com>
2025-02-11 19:14:48 +0000

    List out optimised tests explicitly

--- test/run.py
+++ test/run.py
@@ -100,22 +100,62 @@ assert_dir()
 setup()
 
 epfl = testdir / "EPFL-benchmarks"
+
 test_cases = []
 for category in ["random_control", "arithmetic"]:
     for verilog_file in (epfl / category).glob("*.v"):
         name = verilog_file.stem
         test_cases.append((name, verilog_file, epfl / category / f"{name}.blif"))
 
-best_results = epfl / "best_results"
-for metric in ["depth", "size"]:
-    for category in ["arithmetic", "random_control"]:
-        for verilog_file in (epfl / category).glob("*.v"):
-            name = verilog_file.stem
-            blif_file = best_results / metric / f"{name}_{metric}_2022.blif"
-            if blif_file.exists():
-                test_cases.append((f"{name}-{metric}", verilog_file, blif_file))
-            else:
-                print(f"Skipping {name}-{metric}")
+depth = epfl / "best_results" / "depth"
+size = epfl / "best_results" / "size"
+arithmetic = epfl / "arithmetic"
+random_control = epfl / "random_control"
+
+test_cases += [
+    ("adder-depth",       arithmetic/"adder.v",         depth/"adder_depth_2021.blif"),
+    ("bar-depth",         arithmetic/"bar.v",           depth/"bar_depth_2015.blif"),
+    ("div-depth",         arithmetic/"div.v",           depth/"div_depth_2021.blif"),
+    ("hyp-depth",         arithmetic/"hyp.v",           depth/"hyp_depth_2021.blif"),
+    ("log2-depth",        arithmetic/"log2.v",          depth/"log2_depth_2022.blif"),
+    ("max-depth",         arithmetic/"max.v",           depth/"max_depth_2021.blif"),
+    ("multiplier-depth",  arithmetic/"multiplier.v",    depth/"multiplier_depth_2022.blif"),
+    ("sin-depth",         arithmetic/"sin.v",           depth/"sin_depth_2021.blif"),
+    ("sqrt-depth",        arithmetic/"sqrt.v",          depth/"sqrt_depth_2021.blif"),
+    ("square-depth",      arithmetic/"square.v",        depth/"square_depth_2022.blif"),
+    ("arbiter-depth",     random_control/"arbiter.v",   depth/"arbiter_depth_2021.blif"),
+    ("cavlc-depth",       random_control/"cavlc.v",     depth/"cavlc_depth_2018.blif"),
+    ("ctrl-depth",        random_control/"ctrl.v",      depth/"ctrl_depth_2017.blif"),
+    ("dec-depth",         random_control/"dec.v",       depth/"dec_depth_2018.blif"),
+    ("i2c-depth",         random_control/"i2c.v",       depth/"i2c_depth_2022.blif"),
+    ("int2float-depth",   random_control/"int2float.v", depth/"int2float_depth_2018.blif"),
+    ("mem_ctrl-depth",    random_control/"mem_ctrl.v",  depth/"mem_ctrl_depth_2018.blif"),
+    ("priority-depth",    random_control/"priority.v",  depth/"priority_depth_2022.blif"),
+    ("router-depth",      random_control/"router.v",    depth/"router_depth_2021.blif"),
+    ("voter-depth",       random_control/"voter.v",     depth/"voter_depth_2021.blif"),
+
+    ("adder-size",       arithmetic/"adder.v",         size/"adder_size_2022.blif"),
+    ("bar-size",         arithmetic/"bar.v",           size/"bar_size_2015.blif"),
+    ("div-size",         arithmetic/"div.v",           size/"div_size_2021.blif"),
+    ("hyp-size",         arithmetic/"hyp.v",           size/"hyp_size_2021.blif"),
+    ("log2-size",        arithmetic/"log2.v",          size/"log2_size_2021.blif"),
+    ("max-size",         arithmetic/"max.v",           size/"max_size_2018.blif"),
+    ("multiplier-size",  arithmetic/"multiplier.v",    size/"multiplier_size_2022.blif"),
+    ("sin-size",         arithmetic/"sin.v",           size/"sin_size_2021.blif"),
+    ("sqrt-size",        arithmetic/"sqrt.v",          size/"sqrt_size_2021.blif"),
+    ("square-size",      arithmetic/"square.v",        size/"square_size_2021.blif"),
+
+    ("arbiter-size",     random_control/"arbiter.v",   size/"arbiter_size_2022.blif"),
+    ("cavlc-size",       random_control/"cavlc.v",     size/"cavlc_size_2018.blif"),
+    ("ctrl-size",        random_control/"ctrl.v",      size/"ctrl_size_2017.blif"),
+    ("dec-size",         random_control/"dec.v",       size/"dec_size_2018.blif"),
+    ("i2c-size",         random_control/"i2c.v",       size/"i2c_size_2018.blif"),
+    ("int2float-size",   random_control/"int2float.v", size/"int2float_size_2020.blif"),
+    ("mem_ctrl-size",    random_control/"mem_ctrl.v",  size/"mem_ctrl_size_2021.blif"),
+    ("priority-size",    random_control/"priority.v",  size/"priority_size_2021.blif"),
+    ("router-size",      random_control/"router.v",    size/"router_size_2018.blif"),
+    ("voter-size",       random_control/"voter.v",     size/"voter_size_2022.blif"),
+]
 
 with ThreadPoolExecutor(max_workers=MAX_CONCURRENT_TESTS) as executor:
     futures = {executor.submit(veratest, *args): args[0] for args in test_cases}
