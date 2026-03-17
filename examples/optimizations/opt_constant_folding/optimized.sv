1a6124e0a7d22334c7d547fdea49115ebf3956a7
Michalis Pardalos <me@mpardalos.com>
2026-03-17 17:24:56 -0400

    Remove hand-optimized test cases that yosys can figure out

--- examples/optimizations/opt_constant_folding/optimized.sv
+++
@@ -1,7 +0,0 @@
-module constant_folding_optimized (
-    input  logic [7:0] x,
-    input  logic [7:0] y,
-    output logic [7:0] result
-);
-    assign result = x + 8'd8 + y + 8'd8 + 8'd8;
-endmodule
