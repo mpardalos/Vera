aa36ff3d57b7953d1e750c13a7405e120047aa35
Michalis Pardalos <me@mpardalos.com>
2026-03-24 12:07:04 +0000

    [benchmarks] Turn bitselect, conditional, rangeselect, multiplier into templates

--- examples/rangeselect/rangeselect.sv
+++
@@ -1,3 +0,0 @@
-module rangeselect(input [31:0] in, output [7:0] out);
-   assign out = in[23:16];
-endmodule // rangeselect
--- examples/rangeselect/shift.sv
+++
@@ -1,3 +0,0 @@
-module rangeselect(input [31:0] in, output [7:0] out);
-   assign out = in >> 16;;
-endmodule // rangeselect
