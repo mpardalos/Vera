6db8789809da0cd628c1d3a327871d8e13622f86
Michalis Pardalos <me@mpardalos.com>
2026-04-30 19:35:01 +0100

    [bench] Format shakefile

--- benchtest/Shakefile.hs
+++ benchtest/Shakefile.hs
@@ -1,50 +1,50 @@
 #!/usr/bin/env runhaskell
 
-{-# LANGUAGE PatternSynonyms #-}
-{-# LANGUAGE ViewPatterns #-}
-{-# LANGUAGE MultiWayIf #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE NoFieldSelectors #-}
 {-# LANGUAGE OverloadedRecordDot #-}
 
+import Control.DeepSeq (NFData)
+import Control.Monad (forM, forM_, guard, join, void, when)
+import Data.Bifunctor (bimap)
+import Data.Binary (Binary)
+import Data.ByteString (ByteString)
+import Data.ByteString qualified as BS
+import Data.ByteString.Char8 qualified as BS8
+import Data.ByteString.Lazy qualified as LBS
+import Data.ByteString.Lazy.Char8 qualified as LBS8
+import Data.Char (isDigit)
+import Data.Data (Typeable)
+import Data.Function ((&))
+import Data.Functor ((<&>))
+import Data.Hashable (Hashable)
+import Data.List (find, intercalate, isInfixOf, isPrefixOf, isSuffixOf, sort, stripPrefix, unsnoc)
+import Data.List.Extra (firstJust)
+import Data.Maybe (fromMaybe, isJust)
+import Data.Text (Text)
+import Data.Text qualified as T
+import Data.Text.Encoding qualified as T
+import Data.Text.IO qualified as T
+import Data.Time.Clock (diffUTCTime, getCurrentTime)
+import Data.Tuple (swap)
+import Debug.Trace
 import Development.Shake
 import Development.Shake.Command
 import Development.Shake.FilePath
 import Development.Shake.Util
-import Data.Tuple (swap)
-import Data.List (isSuffixOf, isInfixOf, isPrefixOf, stripPrefix, unsnoc, find, intercalate, sort)
-import Data.Bifunctor (bimap)
-import Data.Char (isDigit)
-import Data.Maybe (isJust, fromMaybe)
-import Control.Monad (forM, forM_, guard, join, when, void)
-import System.Directory qualified as SD
-import Data.Time.Clock (getCurrentTime, diffUTCTime)
-import Text.Printf (printf)
-import System.Exit(ExitCode(..))
-import Data.Function ((&))
-import Debug.Trace
-import Data.List.Extra (firstJust)
-import Data.Functor ((<&>))
-import Data.ByteString (ByteString)
-import Data.ByteString.Char8 qualified as BS8
-import Data.ByteString qualified as BS
-import Data.ByteString.Lazy qualified as LBS
-import Data.ByteString.Lazy.Char8 qualified as LBS8
-import Data.Text (Text)
-import Data.Text qualified as T
-import Data.Text.IO qualified as T
-import Data.Data (Typeable)
-import Data.Hashable (Hashable)
-import Data.Binary (Binary)
-import Control.DeepSeq (NFData)
 import GHC.Generics (Generic)
-import System.Posix.Resource (setResourceLimit, getResourceLimit, Resource(ResourceOpenFiles), ResourceLimit(..), ResourceLimits(..))
-import qualified Data.Text.Encoding as T
+import System.Directory qualified as SD
+import System.Exit (ExitCode (..))
+import System.Posix.Resource (Resource (ResourceOpenFiles), ResourceLimit (..), ResourceLimits (..), getResourceLimit, setResourceLimit)
+import Text.Printf (printf)
 
 -- | Path to vera binary
 vera :: FilePath
@@ -82,14 +82,14 @@ data BenchmarkResult = BenchmarkResult
   }
 
 main :: IO ()
-main = shakeArgs shakeOptions {shakeThreads=0} $ do
+main = shakeArgs shakeOptions{shakeThreads = 0} $ do
   liftIO $ do
     -- Set open file soft limit to the hard limit
-    ResourceLimits {hardLimit} <- getResourceLimit ResourceOpenFiles
+    ResourceLimits{hardLimit} <- getResourceLimit ResourceOpenFiles
     setResourceLimit ResourceOpenFiles (ResourceLimits hardLimit hardLimit)
 
   -- Sizes which templated examples will be evaluated at
-  addOracle $ \ConfigRunSizes -> pure [4..8]
+  addOracle $ \ConfigRunSizes -> pure [4 .. 8]
   -- Value of --solver= flag for vera
   addOracle $ \ConfigSolver -> pure "cvc5"
   -- Timeout for vera/eqy runs (in seconds)
@@ -105,8 +105,9 @@ main = shakeArgs shakeOptions {shakeThreads=0} $ do
     removeFilesAfter "out/templates" ["//"]
 
   phony "synth" $ do
-    sources <- filter (not . (".synth.sv" `isSuffixOf`))
-      <$> getDirectoryFiles "." ["templates//*.sv"]
+    sources <-
+      filter (not . (".synth.sv" `isSuffixOf`))
+        <$> getDirectoryFiles "." ["templates//*.sv"]
     let targets = map (-<.> "synth.sv") sources
     need targets
 
@@ -115,19 +116,22 @@ main = shakeArgs shakeOptions {shakeThreads=0} $ do
 
   -- Run yosys synthesis. Needs to take priority over the gen_ rule
   -- below, since they both match gen_*/*.synth.sv
-  priority 2 $ "out//*.synth.sv" %> \out -> do
-    let src = dropExtensions out <> ".sv"
-    let log = dropExtensions out <> ".synth.log"
-    need ["templates/synth.tcl", src]
-    yosysTimeout <- askOracle ConfigYosysTimeout
-    cmd_
-      (Traced "yosys")
-      (AddEnv "SV_INPUT" src)
-      (AddEnv "SV_OUTPUT" out)
-      (Timeout yosysTimeout)
-      (FileStdout log)
-      (FileStderr log)
-      "yosys" "-c" "templates/synth.tcl"
+  priority 2 $
+    "out//*.synth.sv" %> \out -> do
+      let src = dropExtensions out <> ".sv"
+      let log = dropExtensions out <> ".synth.log"
+      need ["templates/synth.tcl", src]
+      yosysTimeout <- askOracle ConfigYosysTimeout
+      cmd_
+        (Traced "yosys")
+        (AddEnv "SV_INPUT" src)
+        (AddEnv "SV_OUTPUT" out)
+        (Timeout yosysTimeout)
+        (FileStdout log)
+        (FileStderr log)
+        "yosys"
+        "-c"
+        "templates/synth.tcl"
 
   -- gen_<category>_<N>/<module>.sv -> templates/<category>/<module>.sv.j2
   "out/templates/gen_*/*.sv" %> \out -> do
@@ -138,10 +142,13 @@ main = shakeArgs shakeOptions {shakeThreads=0} $ do
       (Traced "jinja")
       (FileStdout out)
       (FileStderr log)
-      "jinja2" "-D" ("N=" <> show size) template
+      "jinja2"
+      "-D"
+      ("N=" <> show size)
+      template
 
   -- Running vera
-  "//*.vera.smt2" %> \out -> need [ out -<.> "log" ]
+  "//*.vera.smt2" %> \out -> need [out -<.> "log"]
   "//*_vs_*.vera.log" !%> \out [dir, mod1, mod2] -> do
     let smtFile = out -<.> "smt2"
         left = dir </> mod1 <.> "sv"
@@ -196,8 +203,8 @@ main = shakeArgs shakeOptions {shakeThreads=0} $ do
       =<< getDirectoryFiles
         ""
         [ dir <//> ext
-        | dir <- ["../vera", "../bin"],
-          ext <- ["*.v", "*.ml"]
+        | dir <- ["../vera", "../bin"]
+        , ext <- ["*.v", "*.ml"]
         ]
     cmd_ (Cwd "..") "dune" "build"
 
@@ -210,9 +217,12 @@ main = shakeArgs shakeOptions {shakeThreads=0} $ do
       (Traced "jinja")
       (FileStdout out)
       "jinja2"
-      "-D" ("SOLVER=" <> solver)
-      "-D" ("SV_GOLD=" <> (".." </> mod1 <.> "sv"))
-      "-D" ("SV_GATE=" <> (".." </> mod2 <.> "sv"))
+      "-D"
+      ("SOLVER=" <> solver)
+      "-D"
+      ("SV_GOLD=" <> (".." </> mod1 <.> "sv"))
+      "-D"
+      ("SV_GATE=" <> (".." </> mod2 <.> "sv"))
       template
   "//*_vs_*.eqy.log" !%> \out [dir, mod1, mod2] -> do
     let eqyDir = dropExtensions out
@@ -272,35 +282,38 @@ main = shakeArgs shakeOptions {shakeThreads=0} $ do
     (Exit code) <-
       cmd
         (Traced "gnuplot")
-        "gnuplot" "-e"
+        "gnuplot"
+        "-e"
         [ unwords
-          [ "set terminal pdf;"
-          , "set output '" ++ out ++ "';"
-          , "set datafile separator ',';"
-          , "set xlabel 'Bit width';"
-          , "set ylabel 'Time (s)';"
-          , "set title '" ++ title ++ "';"
-          , "set xtics 1;"
-          , "plot '" ++ summaryCSV ++ "' using 1:2 with linespoints title columnheader(2)"
-          , "   , '" ++ summaryCSV ++ "' using 1:3 with linespoints title columnheader(3)"
-          ] ]
+            [ "set terminal pdf;"
+            , "set output '" ++ out ++ "';"
+            , "set datafile separator ',';"
+            , "set xlabel 'Bit width';"
+            , "set ylabel 'Time (s)';"
+            , "set title '" ++ title ++ "';"
+            , "set xtics 1;"
+            , "plot '" ++ summaryCSV ++ "' using 1:2 with linespoints title columnheader(2)"
+            , "   , '" ++ summaryCSV ++ "' using 1:3 with linespoints title columnheader(3)"
+            ]
+        ]
     case code of
       ExitSuccess -> pure ()
       ExitFailure _ ->
         cmd_
           (Traced "gnuplot_dummy")
-          "gnuplot" "-e"
+          "gnuplot"
+          "-e"
           [ unwords
-            [ "set terminal pdf;"
-            , "set output '" ++ out ++ "';"
-            , "set title '" ++ title ++ "';"
-            , "unset border;"
-            , "unset tics;"
-            , "set xrange [0:1];"
-            , "set yrange [0:1];"
-            , "set label 1 'Error: Plot generation failed or missing data' at 0.5, 0.5 center font ',14';"
-            , "plot NaN notitle"
-            ]
+              [ "set terminal pdf;"
+              , "set output '" ++ out ++ "';"
+              , "set title '" ++ title ++ "';"
+              , "unset border;"
+              , "unset tics;"
+              , "set xrange [0:1];"
+              , "set yrange [0:1];"
+              , "set label 1 'Error: Plot generation failed or missing data' at 0.5, 0.5 center font ',14';"
+              , "plot NaN notitle"
+              ]
           ]
 
   let runEquivalenceCheckers dir modA modB = do
@@ -332,7 +345,7 @@ main = shakeArgs shakeOptions {shakeThreads=0} $ do
   let
     blifToVerilog :: FilePath -> FilePath -> Action ()
     blifToVerilog from to = do
-      need [ from ]
+      need [from]
       let log = to <.> "log"
       -- rename -hide gives the internal wires of the module private
       -- names. This prevents eqy from trying to match them up with
@@ -343,33 +356,37 @@ main = shakeArgs shakeOptions {shakeThreads=0} $ do
       -- to, so we only run the rename here (on the blif), and not on
       -- the orig file. If we compare two Verilogs which both come
       -- from blifs from this function, there might be trouble.
-      cmd_ (FileStdout log) (FileStderr log)
-        "yosys" "--commands"
-        [ printf "read_blif %s; rename -hide *; write_verilog %s" from to :: String ]
+      cmd_
+        (FileStdout log)
+        (FileStderr log)
+        "yosys"
+        "--commands"
+        [printf "read_blif %s; rename -hide *; write_verilog %s" from to :: String]
       trackWrite [to]
 
-  -- | Rename the ports in the target file to match those in the source file
+  -- \| Rename the ports in the target file to match those in the source file
   let
     renamePorts :: FilePath -> FilePath -> Action ()
     renamePorts base target = do
-        let portFilter = ".design.members[1].body.members[] | select(.kind == \"Port\") | .name"
-        Stdout (basePortsLines :: ByteString) <- cmd Shell (printf "slang -q --ast-json=- %s | jq -r '%s'" base portFilter :: String)
-        Stdout (targetPortsLines :: ByteString) <- cmd Shell (printf "slang -q --ast-json=- %s | jq -r '%s'" target portFilter :: String)
-        let basePorts :: [ByteString] = BS8.lines basePortsLines
-        let targetPorts :: [ByteString] = BS8.lines targetPortsLines
-        if (sort basePorts /= sort targetPorts)
-          then do
-            -- The port we are renaming to needs to be escaped. This:
-            --   a) Assumes that it is not already escaped in what we get from slang
-            --   b) Adds a space afterwards so that the escaping doesn't "eat" any chars that happen to be after the identifier
-            let portPairs = [ (BS8.pack "\\\\" <> basePort <> BS8.pack " ", targetPort)
-                            | (basePort, targetPort) <- zip basePorts targetPorts
-                            ]
-            let sedScript = BS8.unlines [BS8.pack "s/\\<" <> from <> BS8.pack "\\>/" <> to <> BS8.pack "/g" | (to, from) <- portPairs]
-            liftIO $ BS.writeFile (target <.> "sed") sedScript
-            cmd_ "sed" ["-i", "-f", target <.> "sed", target]
-            liftIO $ T.appendFile target (T.pack "\n// PORTS RENAMED\n")
-          else liftIO $ T.appendFile target (T.pack "\n// PORTS NOT RENAMED\n")
+      let portFilter = ".design.members[1].body.members[] | select(.kind == \"Port\") | .name"
+      Stdout (basePortsLines :: ByteString) <- cmd Shell (printf "slang -q --ast-json=- %s | jq -r '%s'" base portFilter :: String)
+      Stdout (targetPortsLines :: ByteString) <- cmd Shell (printf "slang -q --ast-json=- %s | jq -r '%s'" target portFilter :: String)
+      let basePorts :: [ByteString] = BS8.lines basePortsLines
+      let targetPorts :: [ByteString] = BS8.lines targetPortsLines
+      if (sort basePorts /= sort targetPorts)
+        then do
+          -- The port we are renaming to needs to be escaped. This:
+          --   a) Assumes that it is not already escaped in what we get from slang
+          --   b) Adds a space afterwards so that the escaping doesn't "eat" any chars that happen to be after the identifier
+          let portPairs =
+                [ (BS8.pack "\\\\" <> basePort <> BS8.pack " ", targetPort)
+                | (basePort, targetPort) <- zip basePorts targetPorts
+                ]
+          let sedScript = BS8.unlines [BS8.pack "s/\\<" <> from <> BS8.pack "\\>/" <> to <> BS8.pack "/g" | (to, from) <- portPairs]
+          liftIO $ BS.writeFile (target <.> "sed") sedScript
+          cmd_ "sed" ["-i", "-f", target <.> "sed", target]
+          liftIO $ T.appendFile target (T.pack "\n// PORTS RENAMED\n")
+        else liftIO $ T.appendFile target (T.pack "\n// PORTS NOT RENAMED\n")
 
   "out/EPFL-benchmarks/*/*/orig.sv" !%> \out [category, name] -> do
     let src = "EPFL-benchmarks" </> category </> name -<.> "v"
@@ -383,27 +400,27 @@ main = shakeArgs shakeOptions {shakeThreads=0} $ do
 
   "out/EPFL-benchmarks/*/*/best_size.sv" !%> \out [category, name] -> do
     let base = "EPFL-benchmarks" </> category </> name -<.> "v"
-    [src] <- getDirectoryFiles "" [ printf "EPFL-benchmarks/best_results/size/%s_size_*.blif" name ]
+    [src] <- getDirectoryFiles "" [printf "EPFL-benchmarks/best_results/size/%s_size_*.blif" name]
     blifToVerilog src out
     renamePorts base out
 
   "out/EPFL-benchmarks/*/*/best_depth.sv" !%> \out [category, name] -> do
     let base = "EPFL-benchmarks" </> category </> name -<.> "v"
-    [src] <- getDirectoryFiles "" [ printf "EPFL-benchmarks/best_results/depth/%s_depth_*.blif" name ]
+    [src] <- getDirectoryFiles "" [printf "EPFL-benchmarks/best_results/depth/%s_depth_*.blif" name]
     blifToVerilog src out
     renamePorts base out
 
   phony "clean-epfl" $ removeFilesAfter "out/EPFL-benchmarks" ["//"]
 
-  phony "epfl" $ need ["out/EPFL-benchmarks/summary.csv" ]
-  "out/EPFL-benchmarks/summary.csv" %> \out  -> do
-    verilogFiles <- getDirectoryFiles "" [ "EPFL-benchmarks/arithmetic/*.v", "EPFL-benchmarks/random_control/*.v"]
+  phony "epfl" $ need ["out/EPFL-benchmarks/summary.csv"]
+  "out/EPFL-benchmarks/summary.csv" %> \out -> do
+    verilogFiles <- getDirectoryFiles "" ["EPFL-benchmarks/arithmetic/*.v", "EPFL-benchmarks/random_control/*.v"]
     let targets =
-         [ (category, name, "orig", target)
-         | verilogFile <- verilogFiles
-         , target <- ["orig_blif", "best_size", "best_depth"]
-         , let Just [ category, name ] = filePattern "EPFL-benchmarks/*/*.v" verilogFile
-         ]
+          [ (category, name, "orig", target)
+          | verilogFile <- verilogFiles
+          , target <- ["orig_blif", "best_size", "best_depth"]
+          , let Just [category, name] = filePattern "EPFL-benchmarks/*/*.v" verilogFile
+          ]
     lines <- forP targets $ \(category, name, modA, modB) -> do
       let dir = "out/EPFL-benchmarks" </> category </> name
       result <- runEquivalenceCheckers dir modA modB
@@ -435,21 +452,23 @@ main = shakeArgs shakeOptions {shakeThreads=0} $ do
 
 -- | Like (%>), but you also get the list of matched components
 (!%>) :: FilePath -> (FilePath -> [FilePath] -> Action ()) -> Rules ()
-(!%>) pat act = pat %> \target ->
-  let Just split = filePattern pat target
-  in act target split
+(!%>) pat act =
+  pat %> \target ->
+    let Just split = filePattern pat target
+     in act target split
 
 -- Split the list on the first instance of the separator
-splitOn :: Eq a => a -> [a] -> ([a], [a])
+splitOn :: (Eq a) => a -> [a] -> ([a], [a])
 splitOn p lst =
   ( takeWhile (/= p) $ lst
   , tailSafe . dropWhile (/= p) $ lst
   )
-  where tailSafe [] = []
-        tailSafe (x:xs) = xs
+ where
+  tailSafe [] = []
+  tailSafe (x : xs) = xs
 
 -- Split the list on the last instance of the separator
-splitOnLast :: Eq a => a -> [a] -> ([a], [a])
+splitOnLast :: (Eq a) => a -> [a] -> ([a], [a])
 splitOnLast p = swap . bimap reverse reverse . splitOn p . reverse
 
 -- | gen_<category>_<N> -> Just (<category>, <N>)
@@ -476,10 +495,10 @@ pattern Snoc xs x <- (unsnoc -> Just (xs, x))
 
 allPairs :: [a] -> [(a, a)]
 allPairs [] = []
-allPairs (x:xs) = map (x,) xs ++ allPairs xs
+allPairs (x : xs) = map (x,) xs ++ allPairs xs
 
 findPrefixedLine :: Text -> Text -> Text
 findPrefixedLine prefix =
   fromMaybe (T.pack "-")
-  . firstJust (T.stripPrefix prefix)
-  . T.lines
+    . firstJust (T.stripPrefix prefix)
+    . T.lines
