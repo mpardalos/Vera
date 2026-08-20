da8d7fc23bf810896659fb4f5b7e859bd653c5e3
Michalis Pardalos <me@mpardalos.com>
2026-08-20 11:07:15 +0100

    Add timestamp to all VERA_TRACE messages

--- bin/extracted/ExtractionUtils.ml
+++ bin/extracted/ExtractionUtils.ml
@@ -1,34 +1,28 @@
 let trace_enabled = ref false
 let trace_indent = ref 0
-let trace_start = ref 0.0
-let trace_started = ref false
+let trace_start = Unix.gettimeofday ()
 
-let make_prefix n marker =
+let make_prefix n marker fmt =
   let buf = Buffer.create (n * 3 + 4) in
   for _ = 1 to n do Buffer.add_string buf "| " done;
   Buffer.add_string buf marker;
-  Buffer.contents buf
+  Printf.fprintf fmt "[%8.3f] %s" (Unix.gettimeofday () -. trace_start) (Buffer.contents buf)
 
 let my_rocq_traceBracket msg f =
   if not !trace_enabled then f ()
   else begin
-    if not !trace_started then begin
-      trace_start := Unix.gettimeofday ();
-      trace_started := true
-    end;
-    let elapsed () = Unix.gettimeofday () -. !trace_start in
     let n = !trace_indent in
-    Printf.eprintf "[%8.3f] %s %s\n%!" (elapsed ()) (make_prefix n "+") msg;
+    Printf.eprintf "%t %s\n%!" (make_prefix n "/") msg;
     incr trace_indent;
     let t0 = Unix.gettimeofday () in
     let result = f () in
     let took = Unix.gettimeofday () -. t0 in
     decr trace_indent;
-    Printf.eprintf "[%8.3f] %s %s (%.3fs)\n%!" (elapsed ()) (make_prefix n "+") msg took;
+    Printf.eprintf "%t %s (%.3fs)\n%!" (make_prefix n "\\") msg took;
     result
   end
 
 let my_rocq_trace msg f =
   if !trace_enabled
-    then Printf.eprintf "%s %s\n%!" (make_prefix !trace_indent ">") msg;
+    then Printf.eprintf "%t %s\n%!" (make_prefix !trace_indent ">") msg;
   f ()
